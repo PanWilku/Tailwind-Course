c6b0fbe1a097f1f5fb15bcebd228a28a5e0f7e71
Marcin Wilk <marcin.wilk.503@gmail.com>
2024-12-09 23:40:44 +0900

    Sections of website added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { CustomerReviews, Footer, Hero, PopularProducts, Services,
+  SpecialOffer, Subscribe, SuperQuality
+ } from "./sections"
+
 
 
 function App() {
@@ -5,11 +9,46 @@ function App() {
   return (
 <>
 
-<h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="relative">
+      {/* Nav */}
+      <section
+      className="xl:padding-1 wide:padding-r padding-b">
 
+      </section>
+      <section
+      className="padding">
+        <Hero />
+      </section>
+      <section
+      className="padding">
+        <PopularProducts />
+      </section>
+      <section
+      className="padding">
+        <SuperQuality />
+      </section>
+      <section
+      className="padding-x py-10">
+        <Services />
+      </section>
+      <section
+      className="padding">
+        <SpecialOffer />
+      </section>
+      <section
+      className=" bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section
+      className="padding-x sm:py32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section
+      className="padding-x padding-t bg-black pb-8">
+        <Footer />
+      </section>
 
+    </main>
 
 
 </>
