6470cccd016bcf146bc9015877b0a90217577ae6
Marcin Wilk <marcin.wilk.503@gmail.com>
2024-12-11 07:44:25 +0900

    heading and buttons added to hero page

---
+++ src/components/Button.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = (props) => {
+  return (
+    <button className='flex justify-center
+    items-center
+    gap-2 px-7 py-4
+    font-montserrat text-lg
+    leading-none  bg-coral-red rounded-full
+    border-coral-red'>
+        {props.label}
+        <img 
+        src={props.iconURL}
+        alt='arrow right icon'
+        className='ml-2 rouunded-full w-5 h-5'/>
+    </button>
+  )
+}
+
+
+Button.propTypes = {
+    label: PropTypes.string,
+    iconURL: PropTypes.string
+};
+
+
+export default Button
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Button from '../components/Button'
+import { arrowRight } from '../assets/icons'
+import { statistics } from '../assets/constants'
 
 const Hero = () => {
+  console.log(typeof arrowRight)
   return (
-    <div>Hero</div>
+    
+    <section
+    id='home'
+    className='w-full
+    flex flex-col xl:flex-row
+    justify-center min-h-screen
+    gap-10 max-container border-2 border-red-400'>
+
+      {/* w-full sets the width of the div to 100% by default, unless overridden (e.g., by xl:w-2/5). */}
+      <div className='xl:w-2/5 flex 
+      flex-col items-start w-full max-xl:padding-x pt-28
+      border-2 border-green-200'>
+        <p>Our Summer collections</p>
+        <h1><span>The New Arrival</span>
+        <br />
+        <span>Nike</span> Shoes
+        </h1>
+        <p>Discover stylish Nike arrivals, quality comfort, add innovation for your active life.</p>
+        <Button label="Shop now"
+        iconURL={arrowRight}>
+
+        </Button>
+        <div className='flex flex-row max-sm:flex-col
+         border-4 border-red-500'>
+          {statistics.map((item) => (
+            <div className='sm:pr-16 sm:mt-10 max-sm:py-5'>
+              <p className='leading-tight font-bold text-3xl font-montserrat'>{item.value}</p>
+              <p className='leading-tight font-montserrat font-light'>{item.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+    </section>
   )
 }
 
