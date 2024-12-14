84e60c7ff2af4fdf6e21a1c79b8a2ba93b36847a
Marcin Wilk <marcin.wilk.503@gmail.com>
2024-12-14 15:12:36 +0900

    Started building Hero section third time, responsiveness improved on pc and mobile

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,33 +1,14 @@
 import React from 'react'
 
-const ShoeCard = ({shoeURL, changeBigShoe, bigShoeImg}) => {
+const ShoeCard = () => {
 
 
-    const handleClick = () => {
-        if(bigShoeImg !== shoeURL.bigShoe) {
-            changeBigShoe(shoeURL.bigShoe)
-        }
-    }
+    return(
+
+        <h1>SIEMA</h1>
+    )
 
 
-
-  return (
-    <div className={`border-2 rounded-xl
-    ${bigShoeImg === shoeURL.bigShoe
-    ? 'border-coral-red'
-    : 'border-transparent'} 
-    cursor-pointer`}
-    onClick={handleClick}>
-        <div className='flex bg-card bg-cover bg-center rounded-xl object-contain p-4
-        w-40 h-40'>
-
-            <img src={shoeURL.thumbnail} alt='shoe collection'
-            className='object-contain'></img>
-
-        </div>
-    </div>
-
-  )
 }
 
 export default ShoeCard
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,29 +1,26 @@
 import Button from '../components/Button'
 import { arrowRight } from '../assets/icons'
-import { shoes, statistics } from '../assets/constants'
-import { bigShoe1, bigShoe2, bigShoe3 } from '../assets/images'
 import { useState } from 'react'
 import ShoeCard from '../components/ShoeCard'
-
+import { statistics } from '../assets/constants'
 
 
 const Hero = () => {
 
-  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
 
   return (
     
     <section
     id='home'
     className='w-full
-    flex flex-col xl:flex-row
+    flex flex-col lg:flex-row
     min-h-screen xl:justify-center
     max-container'>
 
       {/* w-full sets the width of the div to 100% by default, unless overridden (e.g., by xl:w-2/5). */}
       <div className='relative xl:w-2/5 flex 
       flex-col items-start w-full max-xl:padding-x
-      justify-start pt-64'>
+      justify-start pt-64 border-2 border-red-400'>
         <p className='text-2xl text-orange-400 pb-8 pt-10'>Our Summer collections</p>
         <h1 className='text-8xl font-bold pb-4
         xl:whitespace-nowrap bg-white relative z-10 pr-10'><span className=''>The New Arrival</span>
@@ -37,7 +34,7 @@ const Hero = () => {
         </Button>
         <div className='flex flex-row max-sm:flex-col'>
           {statistics.map((item) => (
-            <div className='sm:pr-16 sm:mt-10 max-sm:py-5'>
+            <div key={item.label} className='sm:pr-16 sm:mt-10 max-sm:py-5'>
               <p className='leading-tight font-bold text-4xl font-montserrat'>{item.value}</p>
               <p className='leading-tight font-montserrat font-light'>{item.label}</p>
             </div>
@@ -45,26 +42,9 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className='flex flex-1 flex-col justify-center
-      items-center xl:min-h-full
-      bg-primary bg-hero bg-cover bg-center '>
-            <img src={bigShoeImg} width={610} className=' object-contain relative z-10 pt-20 mb-10'></img>
+      
+      <div className='w-full  border-2 border-green-400'>
 
-        <div className='flex gap-10 max-sm:flex-wrap max-sm:justify-center'>
-          {shoes.map((shoe) => (
-            
-            <div key={shoe.id}>
-            <ShoeCard
-            shoeURL={shoe}
-            changeBigShoe={(shoe) => setBigShoeImg(shoe)}
-            bigShoeImg={bigShoeImg}
-            >
-            </ShoeCard>
-            </div>
-          ))}
-        </div>
-
-          
 
 
 
