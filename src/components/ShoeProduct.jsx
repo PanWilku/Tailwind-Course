0985ce64b6cff258be6c710b7fb57d7aebd9ea2d
Marcin Wilk <marcin.wilk.503@gmail.com>
2024-12-17 23:51:01 +0900

    simple Popular items section added

---
+++ src/components/ShoeProduct.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { star } from '../assets/icons'
+
+const ShoeProduct = ({shoe}) => {
+  return (
+    <div className="flex flex-col gap-1">
+        <img className='max-w-52' src={shoe.imgURL} alt='shoe-product'></img>
+        <div className='flex items-center gap-2'>
+            <img src={star} width={20} height={20} className='mt-2 mb-2'></img>
+            <p className='font-montserrat text-slate-gray'>(4.5)</p>
+        </div>
+
+        <p className='font-bold'>{shoe.name}</p>
+        <p className='text-orange-600 font-bold'>{shoe.price}</p>
+
+    </div>
+  )
+}
+
+export default ShoeProduct
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { products } from '../assets/constants'
+import ShoeProduct from '../components/ShoeProduct'
 
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id='products'>
+    <div className='flex flex-col border-2 border-yellow-200'>
+      <h1 className="text-4xl font-bold pb-4">Our <span className='text-coral-red'>Popular</span> Products</h1>
+      <p className='pb-8 max-w-md text-slate-gray opacity-80 font-montserrat'>Experience top-notch quality and style with our sought after selections. Discover a world of comfort, design and value</p>
+
+
+      <div className='flex flex-row gap-12 flex-wrap justify-center items-center'>
+        {products.map((item) => (
+          <ShoeProduct
+          shoe={item}
+          >
+          </ShoeProduct>
+        ))}
+
+
+      </div>
+
+
+    </div>
+    </section>
   )
+  
 }
 
 export default PopularProducts
